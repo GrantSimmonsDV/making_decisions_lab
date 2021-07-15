01a8ed10902aeeda6921701a20023a8f27423a8e
GrantSimmonsDV <gsimmons.dv@gmail.com>
2021-07-14 20:04:47 -0600

    finished last problem

--- making-decisions/lab.js
+++ making-decisions/lab.js
@@ -281,3 +281,16 @@ console.log(text)
 */
 
 //Code Here
+// let result = [...range(1,100)]
+
+for(let i=0; i <=100; i++){
+  if (i % 3 === 0){
+    console.log('Dev')
+  } else if (i % 5 === 0){
+    console.log('mountain')
+  } else if (i % 5 === 0 && i % 3 === 0){
+    console.log('Devmountain') 
+   } else if (i % 5 !==0 && i % 3 !== 0){
+      console.log([i])
+    }
+  }
